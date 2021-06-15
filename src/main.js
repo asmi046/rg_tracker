f62020983ac0ca41fe1efad0df8b35af8afba02e
asmi046 <asmi046@gmail.com>
2021-06-15 19:41:47 +0300

    Подключен роутер и вюих

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
+import "@/assets/css/style.css"
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+
+Vue.use(Vuex)
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
