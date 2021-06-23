c3d6156541c6eed33e36ca0ea044a0fd0ba1c312
asmi046 <asmi046@gmail.com>
2021-06-23 14:48:12 +0300

    Qr код + крпасивая кнопка

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import "@/assets/css/style.css"
 import Vue from 'vue'
 import App from './App.vue'
-import Vuex from 'vuex'
+import store from './store'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import './registerServiceWorker'
 
-Vue.use(Vuex)
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
